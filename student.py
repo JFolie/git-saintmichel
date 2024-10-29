072dc51002b4969834c141e0552ba92fbcb6177a
Julian Folie <julian.folie@saintmichelannecy.fr>
2024-10-29 16:06:48 +0100

    first commit

---
+++ student.py
@@ -0,0 +1,17 @@
+class Student: #creation of a student class
+	def __init__(self, grades = []):
+		self.grades = grades
+		self.count_grades = len(grades)
+		if len(grades) == 0 :
+			self.academic_average = 0
+		else :
+			self.academic_average =  sum(grades) / len(grades)
+
+	def add_grade(self, grade) :
+		if  (grade < 0) | (grade > 20) :
+			raise InvalidGrade("Grade should be between 0 and 20")
+		else :
+			self.grades.append(grade)
+
+class InvalidGrade(Exception):
+	pass
